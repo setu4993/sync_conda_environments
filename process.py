3cb96cbac2506c6c53f0d139b92e6c53474250b2
Setu Shah <setu4993@yahoo.co.in>
2019-10-20 16:33:21 -0700

    Rename fn, make errors clear

--- process.py
+++ process.py
@@ -2,8 +2,8 @@ from subprocess import run
 from typing import List
 
 
-def run_command(command: List[str]) -> str:
+def run_subprocess(command: List[str]) -> str:
     proc = run(command, capture_output=True)
     assert proc.args == command, f"Command executed process different from the command passed."
-    assert proc.returncode == 0, f"Error in running sub-process, sub-process returned with a non-zero exit code {proc.returncode}."
+    assert proc.returncode == 0, f"Error in running sub-process, sub-process returned with a non-zero exit code {proc.returncode} for command {command} with error {proc.stderr}."
     return proc.stdout
