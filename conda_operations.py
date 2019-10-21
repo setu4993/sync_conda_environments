65f0f763800f2e863116cb2c5ce9450cc681c5fb
Setu Shah <setu4993@yahoo.co.in>
2019-10-20 23:28:28 -0700

    Update export to include name of env

--- conda_operations.py
+++ conda_operations.py
@@ -9,9 +9,9 @@ from process import run_subprocess
 BASE_CONDA_ENV_COMMAND = ['conda', 'env']
 
 
-def _export_env(filename: str) -> str:
+def _export_env(filename: str, env_name: str) -> str:
     command = copy(BASE_CONDA_ENV_COMMAND)
-    command += ['export']
+    command += ['export', '-n', env_name]
     command += _file_command(filename)
     return run_subprocess(command)
 
@@ -33,7 +33,7 @@ def _create_update_env(filename: str, existing_envs: List[str]) -> str:
 def export_envs(envs: List[str], output_path: str):
     for env in envs:
         env_file = join(output_path, env) + '.yml'
-        _export_env(filename=env_file)
+        _export_env(filename=env_file, env_name=env)
         assert isfile(env_file), f"Output file for env {env} not created."
         logging.info(f'Exported file {env_file} for env named {env}.')
 
