912e61a01bc2a643575f328df0519741e7b09be4
Setu Shah <setu4993@yahoo.co.in>
2019-11-09 22:48:30 -0800

    Add tests

--- tests/test_conda_helpers.py
+++ tests/test_conda_helpers.py
@@ -1,8 +1,23 @@
 from typing import List
 from ..helpers.conda import *
+from pathlib import Path
 
 
 def test_list_envs():
     envs = list_envs()
     assert isinstance(envs, List)
     assert len(envs) >= 0
+
+
+def test_export_envs():
+    envs = ['base']
+    output_path = Path('.')
+    result = export_envs(envs, output_path)
+    assert result
+
+
+def test_create_update_envs():
+    envs = ['base']
+    env_files = [Path('.').joinpath('base.yml')]
+    result = create_update_envs(envs, env_files)
+    assert result
--- tests/test_file_helpers.py
+++ tests/test_file_helpers.py
@@ -1,5 +1,4 @@
 from typing import List
-from types import GeneratorType
 from pathlib import Path
 
 from ..helpers.file import *
@@ -8,6 +7,4 @@ from ..helpers.file import *
 def test_list_envs():
     path = Path('.')
     files = glob_env_files(path)
-    assert isinstance(files, GeneratorType)
-    files = [file for file in files]
     assert isinstance(files, List)
