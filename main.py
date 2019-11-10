f4b7ffa40ecef7d1e3a317722c3037fe0d6c5379
Setu Shah <setu4993@yahoo.co.in>
2019-11-09 22:49:58 -0800

    Add logging using loguru, arg handling

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
+import argparse
 import pathlib
+from loguru import logger
 
 from helpers.conda import create_update_envs, export_envs, list_envs
 from helpers.file import glob_env_files
 
-if __name__ == "__main__":
-    # TODO: Handle args.
-    export = True
-    update = False
-    output_dir = "."
 
-    output_path = pathlib.Path(output_dir)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--update", help="Should the conda environments be updated?", action='store_true', default=False)
+    parser.add_argument("-o", "--output-dir", help="Output directory to store or load environment files from.", type=str, default=".")
 
-    assert output_path.exists(), "Output directory doesn't exist"
+    args, _ = parser.parse_known_args()
+
+    if not args.update:
+        export, update = True, False
+    else:
+        export, update = False, True
+
+    output_path = pathlib.Path(args.output_dir)
+    assert output_path.exists() and output_path.is_dir(), "Output directory doesn't exist."
+    logger.info(f'Using directory {output_path}.')
 
     envs = list_envs()
+    logger.info(f'{len(envs)} environments found. They are: {envs}.')
 
     if export:
-        export_envs(envs=envs, output_path=output_path)
+        logger.info('Executing export of conda environments.')
+        result = export_envs(envs=envs, output_path=output_path)
+        assert result, "Export didn't complete successfully."
 
     if update:
+        logger.info('Executing update of conda environments.')
         env_files = glob_env_files(output_path)
-        create_update_envs(envs=envs, env_files=env_files)
+        result = create_update_envs(envs=envs, env_files=env_files)
+        assert result, "Create / update didn't complete successfully."
+
+
+if __name__ == "__main__":
+    main()
